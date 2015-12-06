1cb379ed46a4ef992a27b2e1053c3c531dee1fe8
Ing. Jan Kaláb <pitlicek@gmail.com>
2015-12-06 19:53:11 +0100

    First 2 days

---
+++ 2.py
@@ -0,0 +1,10 @@
+with open("adventofcode2.in", "r") as f:
+	presents = f.readlines()
+	paper = 0
+	ribbon = 0
+	for present in presents:
+		edges = sorted(map(int, present.strip().split("x")))
+		print(edges)
+		paper += 2 * (edges[0] * edges[1] + edges[1] * edges[2] + edges[0] * edges[2]) + edges[0] * edges[1]
+		ribbon += edges[0] + edges[0] + edges[1] + edges[1] + edges[0] * edges[1] * edges[2]
+	print(paper, ribbon)
